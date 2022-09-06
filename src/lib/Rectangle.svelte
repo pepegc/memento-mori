8906bade5724211cb40c7c2d9de6a9cd528ed24b
pepegc <jpgarcia90@gmail.com>
2022-09-06 17:37:32 -0400

    safari shenanigans

--- src/lib/Rectangle.svelte
+++ src/lib/Rectangle.svelte
@@ -3,6 +3,12 @@
 </script>
 
 
-<svg class='aspect-square'>
+<svg
+    class='aspect-square'
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width=100%
+    height=100%
+>
     <rect x=0 y=0 height=100% width=100% style='stroke:#000000;stroke-width:2;fill:{is_filled ? 'none' : '1'}'/>
 </svg>
